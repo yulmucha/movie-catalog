8b4ab2306af7cc308bb948fcb4d3b5f0153a946f
Yul <notkhaki@gmail.com>
2020-05-02 23:26:50 +0900

    Get movie data through API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  // componentDidMount() {
+  //   console.log(`didMount`);
+  //   const moviesPromise = new Promise((resolve, reject) => {
+  //     const response = Axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+  //     resolve(response);
+  //   });
+
+  //   moviesPromise.then(response => {
+  //     const { data: { data: { movies } } } = response;
+  //     this.setState({ isLoading: false, movies });
+  //   });
+  // }
+
+  async componentDidMount() {
+    const { data: { data: { movies } } } = await Axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ isLoading: false, movies });
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    console.log(isLoading, movies);
+    return (
+      <div></div>
+    );
+  }
 }
 
 export default App;
