bcb7de2b3a866dfdfe107d523c86404cef9a910f
Yul <notkhaki@gmail.com>
2020-05-02 23:59:49 +0900

    Updates

---
+++ precache-manifest.74247a04b42ae2163c12a3e29a8b0e88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a4c29ce149ea2d25a6216820e26b9d",
+    "url": "/JavaScript/movie-catalog/index.html"
+  },
+  {
+    "revision": "3a1be3c2aee75fa1e0fc",
+    "url": "/JavaScript/movie-catalog/static/js/2.58d37e25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JavaScript/movie-catalog/static/js/2.58d37e25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "170f1733269a8cb673c3",
+    "url": "/JavaScript/movie-catalog/static/js/main.9681874d.chunk.js"
+  },
+  {
+    "revision": "47b1c693369c67c484f8",
+    "url": "/JavaScript/movie-catalog/static/js/runtime-main.ef83d526.js"
+  }
+]);
