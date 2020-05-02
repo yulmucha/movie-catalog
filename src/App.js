9201e3ec87c84e4ad26d5143911564fee28bf889
Yul <notkhaki@gmail.com>
2020-05-02 23:45:03 +0900

    Render movie data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import React from 'react';
 import Axios from 'axios';
+import PropTypes from 'prop-types';
+
+
+function Movie({poster, title, year, genres, summary}) {
+  return (
+    <div>
+      <img src={poster} alt={title} title={title} />
+      <div>{title}</div>
+      <div>{year}</div>
+      <ul>
+        {genres.map((genre, idx) => <li key={idx}>{genre}</li>)}
+      </ul>
+      <div>{summary}</div>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  poster: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+  summary: PropTypes.string
+};
 
 class App extends React.Component {
   state = {
@@ -27,9 +51,21 @@ class App extends React.Component {
 
   render() {
     const { isLoading, movies } = this.state;
-    console.log(isLoading, movies);
     return (
-      <div></div>
+      <div>
+        {
+          isLoading ? "Loading..."
+            : movies.map(movie =>
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                genres={movie.genres}
+                summary={movie.summary}
+                poster={movie.medium_cover_image} />)
+        }
+      </div>
     );
   }
 }
